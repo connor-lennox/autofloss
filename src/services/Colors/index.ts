58cac0eeeb1ee468b549d7cb6991649b0190d9ff
Connor Lennox <clennox99@gmail.com>
2022-06-17 21:50:12 -0400

    correct similarity to have "1" be most similar

--- src/services/Colors/index.ts
+++ src/services/Colors/index.ts
@@ -8,5 +8,5 @@ export type Color = {
 export function colorSimilarity(c1: Color, c2: Color) {
     // Just RGB space difference. Eventually this should
     // probably be done by converting to LAB first.
-    return Math.sqrt((c1.red - c2.red) + (c1.green - c2.green) + (c1.blue - c2.blue))
+    return 1 - (Math.sqrt((c1.red - c2.red)/ 255 + (c1.green - c2.green)/ 255 + (c1.blue - c2.blue)/ 255))
 }
