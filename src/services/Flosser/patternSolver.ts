9b79a7462c3f574ec1f09d13c38927dfc5c9bdcd
Connor Lennox <clennox99@gmail.com>
2022-06-17 21:37:01 -0400

    downsampling image to specific pixel width/height

--- src/services/Flosser/patternSolver.ts
+++ src/services/Flosser/patternSolver.ts
@@ -1,5 +1,8 @@
-import {PixelImage} from "../ImageParser";
+import {downsampleImage, PixelImage} from "../ImageParser";
 
 export const solvePattern = (image: PixelImage, maxColors: number) => {
     console.log("Finding solution for image with " + image.height + " rows and " + image.width + " columns.")
+    let downsampledImage = downsampleImage(image, 30, 30)
+    console.log("Downsampled to 30x30:")
+    console.log(downsampledImage)
 }
--- src/services/ImageParser/index.ts
+++ src/services/ImageParser/index.ts
@@ -1,30 +1,28 @@
 import {Color} from "../Colors";
 
-export class PixelImage {
-    public readonly data: Array<Color>;
-    public readonly width: number;
-    public readonly height: number;
+export type PixelImage = {
+    data: Array<Color>
+    width: number
+    height: number
+}
 
-    constructor(data: Uint8ClampedArray, width: number, height: number) {
-        this.data = data.reduce((all: Array<number[]>, one: number, i: number) => {
-            const ch = Math.floor(i / 4);
-            all[ch] = ([] as number[]).concat((all[ch] || []), one);
-            return all
-        }, [] as number[][]).map(e => {
-            return {
-                red: e[0],
-                blue: e[1],
-                green: e[2],
-                alpha: e[3]
-            }
-        });
-        this.width = width;
-        this.height = height;
-    }
+export const getImagePixel = (image: PixelImage, x: number, y: number): Color => {
+    return image.data[image.width * y + x]
+}
 
-    public getPixel(x: number, y: number): Color {
-        return this.data[this.width * y + x]
-    }
+const flatArrayToColors = (data: Uint8ClampedArray): Array<Color> => {
+    return data.reduce((all: Array<number[]>, one: number, i: number) => {
+        const ch = Math.floor(i / 4);
+        all[ch] = ([] as number[]).concat((all[ch] || []), one);
+        return all
+    }, [] as number[][]).map(e => {
+        return {
+            red: e[0],
+            blue: e[1],
+            green: e[2],
+            alpha: e[3]
+        }
+    });
 }
 
 export const parseImage = (imageURL: string) => {
@@ -39,8 +37,31 @@ export const parseImage = (imageURL: string) => {
 
         ctx!.drawImage(img, 0, 0);
 
-        return new PixelImage(ctx!.getImageData(
-            0, 0, img.width, img.height
-        ).data, img.width, img.height);
+        return {
+            data: flatArrayToColors(ctx!.getImageData(
+                0, 0, img.width, img.height
+                ).data),
+            width: img.width,
+            height: img.height
+        }
     })
+}
+
+export const downsampleImage = (image: PixelImage, newWidth: number, newHeight: number): PixelImage => {
+    let wx = Math.floor(image.width / newWidth);
+    let wy = Math.floor(image.height / newHeight);
+
+    let newColors: Color[] = []
+
+    for(let y = 0; y < newHeight; y++) {
+        for(let x = 0; x < newWidth; x++) {
+            newColors.push(getImagePixel(image, x * wx, y * wy));
+        }
+    }
+
+    return {
+        data: newColors,
+        width: newWidth,
+        height: newHeight
+    }
 }
