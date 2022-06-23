ff9f6435e235812e148c35b26fb15e06a81b4ff4
Connor Lennox <clennox99@gmail.com>
2022-06-22 22:10:34 -0400

    Move pattern viewer into own component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import {parseImage, pixelImageToImageData} from "./services/ImageProcessing";
+import {parseImage} from "./services/ImageProcessing";
 import {solvePattern} from "./services/Flosser/patternSolver.ts";
-import {useEffect, useRef, useState} from "react";
+import {useRef, useState} from "react";
 import FlossUsageTable from "./components/FlossUsageTable";
+import PatternViewer from "./components/PatternViewer";
 
 function App() {
     const [imageData, setImageData] = useState(null);
@@ -13,42 +14,10 @@ function App() {
     const heightInputRef = useRef();
     const maxColorsInputRef = useRef();
 
-    const ImageCanvas = props => {
-        const canvasRef = useRef(null);
-
-        useEffect(() => {
-            const canvas = canvasRef.current
-            const ctx = canvas.getContext('2d')
-            if(patternResult != null) {
-                let canvasWidth = props.width, canvasHeight = props.height
-
-                let finishedImage = patternResult.image;
-                ctx.imageSmoothingEnabled = false;
-                ctx.putImageData(pixelImageToImageData(finishedImage), 0, 0);
-                ctx.drawImage(canvas, 0, 0, finishedImage.width, finishedImage.height, 0, 0, canvasWidth, canvasHeight);
-                let xScale = canvasWidth / finishedImage.width;
-                let yScale = canvasHeight / finishedImage.height;
-
-                for(let x = 0; x < finishedImage.width; x++) {
-                    ctx.moveTo(x * xScale, 0);
-                    ctx.lineTo(x * xScale, canvasHeight);
-                    ctx.stroke();
-                }
-                for(let y = 0; y < finishedImage.height; y++) {
-                    ctx.moveTo(0, y * yScale);
-                    ctx.lineTo(canvasWidth, y * yScale);
-                    ctx.stroke();
-                }
-            }
-        })
-
-        return <canvas ref={canvasRef} {...props}/>
-    }
-
   return (
     <div className="App">
       <header className="App-header">
-          { patternResult != null ? <ImageCanvas width={patternResult.image.width * 10} height={patternResult.image.height * 10}/> : null }
+          { patternResult != null ? <PatternViewer pattern={patternResult} targetWidth={600} targetHeight={600}/> : null }
         <input
           type="file"
           name="imageUpload"
---
+++ src/components/PatternViewer.tsx
@@ -0,0 +1,59 @@
+import React, {useEffect, useRef} from 'react';
+import {PatternResult} from "../services/Flosser/patternSolver";
+import {pixelImageToImageData} from "../services/ImageProcessing";
+
+export type PatterViewerProps = {
+    pattern: PatternResult,
+    targetWidth: number,
+    targetHeight: number
+}
+
+export default function PatternViewer(props: PatterViewerProps) {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    // Match either the targetWidth or targetHeight, whichever leads to a smaller scale factor:
+    let finishedImage = props.pattern.image;
+
+    let widthScale = props.targetWidth / finishedImage.width;
+    let heightScale = props.targetHeight / finishedImage.height;
+
+    let canvasScale = Math.min(widthScale, heightScale)
+
+    let canvasWidth = finishedImage.width * canvasScale, canvasHeight = finishedImage.height * canvasScale;
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const ctx = canvas.getContext('2d');
+            if (ctx == null) throw new Error("failed to get canvas context");
+
+            // Clear anything that used to be on our canvas
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Disable image smoothing: this is pixel art!
+            ctx.imageSmoothingEnabled = false;
+
+            // Draw the image to the canvas once, at its original scale
+            ctx.putImageData(pixelImageToImageData(finishedImage), 0, 0);
+
+            // Then overwrite the canvas with the same image, but expanded to fill the whole canvas
+            ctx.drawImage(canvas, 0, 0, finishedImage.width, finishedImage.height, 0, 0, canvasWidth, canvasHeight);
+            let xScale = canvasWidth / finishedImage.width;
+            let yScale = canvasHeight / finishedImage.height;
+
+            // Draw gridlines
+            for (let x = 0; x < finishedImage.width; x++) {
+                ctx.moveTo(x * xScale, 0);
+                ctx.lineTo(x * xScale, canvasHeight);
+                ctx.stroke();
+            }
+            for (let y = 0; y < finishedImage.height; y++) {
+                ctx.moveTo(0, y * yScale);
+                ctx.lineTo(canvasWidth, y * yScale);
+                ctx.stroke();
+            }
+        }
+    })
+
+    return <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight}/>;
+}
